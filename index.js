47f26739e763b34523a8912c3e5721bc9de0a19e
Jonathan KOROL <korol.jonathan@gmail.com>
2023-07-14 01:12:05 +0200

    Ajout de la création d'ingredient

--- index.js
+++ index.js
@@ -280,4 +280,27 @@ app.post('/verifyToken', (req,res)=>{
         
 })
 
+app.get("/ingredient",(req,res)=>{
+    database.execute("SELECT * FROM ingredient",(error,result)=>{
+        if(error){
+            console.log(error.message);
+            res.status(500).send();
+        }
+        else{
+            res.status(200).json(result);
+        }
+    })
+})
+
+app.post("/ingredient",(req,res)=>{
+    database.execute("INSERT INTO ingredient(label) VALUES (?)",[req.body.ingredient],(error,result)=>{
+        if(error){
+            console.log(error.message);
+            res.status(500).send();
+        }else{
+            res.status(200).json({id:result.insertId,label:req.body.ingredient});
+        }
+    })
+})
+
 app.listen(config.settings.port, () => {console.log("Serveur à l'écoute")});
